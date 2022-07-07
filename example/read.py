ab0b5a2c14fb375262304853278f31282b28bf08
bingtao <1007530194@qq.com>
2022-07-07 11:06:59 +0800

    clear history

---
+++ example/read.py
@@ -0,0 +1,9 @@
+from notesecret import read_secret
+
+read_secret("baidu","secret","accessToken",value="this is accessToken")
+read_secret("baidu","secret","refreshToken",value="this is refreshToken")
+
+print(read_secret("baidu","secret","accessToken"))
+print(read_secret("baidu","secret","refreshToken"))
+
+
