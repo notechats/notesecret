2eb168e9f41087697253aeaf3756510dd1a6c392
bingtao <1007530194@qq.com>
2022-07-29 15:01:51 +0800

    add

--- setup.py
+++ setup.py
@@ -11,13 +11,12 @@ except Exception as e:
     from notebuild.tool import read_version
 
 
-
 version_path = path.join(path.abspath(path.dirname(__file__)), 'script/__version__.md')
 
 version = read_version(version_path)
 
 
-install_requires = ['notebuild']
+install_requires = ['notebuild', 'sqlalchemy']
 
 
 setup(name='notesecret',
